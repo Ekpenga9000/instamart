779164601fe89adb4714e624461ffb26f6f2b886
Louis_Ekpenga <ekpenga9000@gmail.com>
2025-04-13 18:12:52 -0600

    Create scarfolding for seller's profile.

--- frontend/src/pages/CreateListing.jsx
+++ frontend/src/pages/CreateListing.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Link, useLocation } from "react-router-dom";
 import ListingTypes from "../components/ListingTypes";
 import CreatePost from "../components/CreatePostList";
+import { IoMdGlobe } from "react-icons/io";
+import { FaUser } from "react-icons/fa";
 
 const CreateListing = () => {
     const location = useLocation();
@@ -10,11 +12,33 @@ const CreateListing = () => {
   return (
     <section className="grid grid-cols-4">
       <div className="col-span-1 p-4 border-r shadow-sm">
-              {pathname === "/create" && <CreatePost />}
+        {pathname === "/create" && <CreatePost />}
               
-              <div>
-                  <h2>Item for sale</h2>
-              </div>
+        <div>
+          <h2 className="text-2xl font-semibold mb-4">Item for sale</h2>
+          <ul className="flex items-center gap-4">
+            <li className="bg-gray-200 h-[3rem] w-[3rem] flex justify-center items-center rounded-full">
+               <FaUser/>
+            </li>
+            <li>
+              <ul>
+                <li className="font-semibold">
+                  Sample User
+                </li>
+                <li className="flex text-xs items-center gap-1 text-gray-700">
+                 
+                  Creating listing to instaMart
+                  <span>
+                    .
+                  </span>
+                  <span className="flex items-center gap-1">
+                  <IoMdGlobe /> Lethbridge
+                  </span>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </div>
       </div>
       <div className="bg-gray-200 col-span-3 h-screen">
         {pathname === "/create" && <ListingTypes />}
